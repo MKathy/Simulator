4ba66a755cda83e432e5132f16084e776774539b
Katarzyna <kathrine1418@gmail.com>
2018-03-20 09:54:30 +0100

    Delete layout, add play function.

--- layout.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "Year: 4/71\tActions: 1/2\tMoney: 53\tIntelligence: 13\tHappiness: 87" << endl;
-    cout << "\n1. Work\n2. Study\n3. Fun\n4. Ask for promotion\n\n0. Exit" << endl;
-    return 0;
-}
--- main.cpp
+++ main.cpp
@@ -7,35 +7,44 @@ using namespace std;
 
 char choice;
 
+void play()
+{
+
+}
 int main()
 {
-for(;;)
-{
-    cout << "--------------" << endl ;
-    cout << "   Menu:" << endl ;
-    cout << "--------------" << endl ;
-    cout << "1. Play" << endl;
-    cout << "2. Leaderboard" << endl;
-    cout << "3. Game save" << endl;
-    cout << "4. Exit" << endl;
-
-    cout << "Your choice: ";
-
-    choice = getch();
-
-    cout << endl;
-    switch(choice)
+    for(;;)
     {
+        cout << "--------------" << endl ;
+        cout << "   Menu:" << endl ;
+        cout << "--------------" << endl ;
+        cout << "1. Play" << endl;
+        cout << "2. Leaderboard" << endl;
+        cout << "3. Save Game" << endl;
+        cout << "4. Exit" << endl;
+
+        cout << "Your choice: ";
+
+        choice = getch();
+
+        cout << endl;
+        switch(choice)
+        {
         case '1':
-            cout << "Play"; break;
+            system("cls");
+            cout << "Year: 4/71\tActions: 1/2\tMoney: 53\tIntelligence: 13\tHappiness: 87" << endl;
+            cout << "\n1. Work\n2. Study\n3. Fun\n4. Ask for promotion\n\n0. Exit" << endl;
+            break;
         case '2':
-            cout << "Leaderboard"; break;
+            cout << "Leaderboard";
+            break;
         case '3':
-            cout << "Game over"; break;
+            cout << "Save Game";
+            break;
         case '4':
             exit(0);
 
-             break;
+            break;
 
         default:
             cout << "Nie ma takiej opcji w menu!" << endl;
@@ -46,7 +55,7 @@ for(;;)
         if(choice == 0)
             choice = getch();
         system("cls");
-}
+    }
     return 0;
 }
 
