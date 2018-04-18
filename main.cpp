06b5f1625ef6453626ba59f65f26c51215598214
Katarzyna <kathrine1418@gmail.com>
2018-04-18 14:59:29 +0200

    Controller object created.

--- main.cpp
+++ main.cpp
@@ -1,38 +1,10 @@
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
-#include "main_menu.h"
-#include "play.h"
+#include "Controller.h"
 
 using namespace std;
 
 int main()
 {
-    for(;;)
-    {
-        display_main_menu();
-        cout << "Your choice: ";
-        int choice = getch();
-
-        cout << endl;
-        switch(choice)
-        {
-        case '1':
-            play();
-            break;
-        case '2':
-            cout << "Leaderboard";
-            break;
-        case '3':
-            cout << "Load Game";
-            break;
-        case '4':
-            return 0;
-        default:
-            cout << "There is no option!" << endl;
-            break;
-        }
-        getch();
-        system("cls");
-    }
+    Controller controller;
+    controller.run();
 }
